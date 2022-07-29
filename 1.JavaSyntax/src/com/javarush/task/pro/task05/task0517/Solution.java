7ce394011d9d706b1fa751b653e4237b112e4be7
Nikitamusatov <nikitamusatov@gmail.com>
2022-07-29 19:07:32 +0300

    add solution JavaSyntax task 0517

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0517/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0517/Solution.java
@@ -12,14 +12,14 @@ public class Solution {
     public static int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
     public static void main(String[] args) {
-        for (int i = 0; i < array.length; i++) {
             if (array.length % 2 == 0) {
-                int[] arrayOne = Arrays.copyOfRange(array, 0, array.length / 2);
+                result[0] = Arrays.copyOfRange(array, 0, array.length / 2);
+                result[1] = Arrays.copyOfRange(array, array.length / 2, array.length);
             }
             else {
-                int[] arrayTwo = Arrays.copyOfRange(array, array.length / 2 + 1, array.length);
+                result[0] = Arrays.copyOfRange(array, 0, array.length / 2 + 1);
+                result[1] = Arrays.copyOfRange(array, array.length / 2 + 1, array.length);
             }
-        }
         System.out.println(Arrays.deepToString(result));
     }
 }
