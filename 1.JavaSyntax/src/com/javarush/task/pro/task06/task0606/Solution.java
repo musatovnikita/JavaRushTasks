344d14e93f7c990cefe6db49fda4f041f3f2f71d
Nikitamusatov <nikitamusatov@gmail.com>
2022-08-02 14:20:47 +0300

    add solution JavaSyntax task 0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(String str, int number){
+
+    }
+
+    public static void universalMethod(String str, float n){
+
+    }
+
+    public static void universalMethod(double i, float j){
+
+    }
+
+    public static void universalMethod(int[][] value){
+
+    }
+
+    public static void universalMethod(String str, double i){
+
+    }
+
+    public static void universalMethod(int k, double o){
+
+    }
+
+    public static void universalMethod(String str, int[][] next){
+
+    }
+
+    public static void universalMethod(int[] wake){
+
+    }
+
+    public static void universalMethod(String text, int[] array){
+
+    }
+
+    public static void universalMethod(String name, int f, double c) {
+
+    }
+}
