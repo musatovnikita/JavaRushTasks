764cbcefac179348ac4c25ce9129c66ede22bca3
Nikitamusatov <nikitamusatov@gmail.com>
2022-08-02 16:29:51 +0300

    add solution JavaSyntax task 0608

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0608/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task06.task0608;
+
+/* 
+Кубический калькулятор
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(cube(111));
+    }
+
+    public static long cube(long z) {
+        return z = z * z * z;
+
+        }
+    }
+
