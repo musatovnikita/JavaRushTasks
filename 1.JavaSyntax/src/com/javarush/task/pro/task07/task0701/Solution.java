d0896d9fbcf16d62246d05f5cb8276b0305d37b5
Nikitamusatov <nikitamusatov@gmail.com>
2022-08-04 08:27:11 +0300

    add solution JavaSyntax task 0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    boolean a;
+    float b;
+    double c;
+    short d;
+    int e;
+    long f;
+    char g;
+    byte i;
+}
