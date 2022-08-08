e16015ec6c0c71bafd22fa559c52e7b2045db889
Nikitamusatov <nikitamusatov@gmail.com>
2022-08-08 16:30:40 +0300

    add solution JavaSyntax task 0914

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0914/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task09.task0914;
+
+/* 
+Обновление пути
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String path = "/usr/java/jdk1.8/bin/";
+
+        String jdk13 = "jdk-13";
+        System.out.println(changePath(path, jdk13));
+    }
+
+    public static String changePath(String path, String jdk) {
+        int firstJdk = path.indexOf("jdk");
+        int lastJdk = path.indexOf("/", firstJdk);
+        String oldJdk = path.substring(firstJdk, lastJdk);
+        return path.replace(oldJdk, jdk);
+    }
+}
