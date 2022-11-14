195ceab8aadaf2f492139c831b21c08a0fdd8fa8
Nikitamusatov <nikitamusatov@gmail.com>
2022-11-14 12:03:41 +0300

    add solution JavaSyntax task 0103

--- 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -6,7 +6,7 @@ package com.javarush.task.task01.task0103;
 
 public class Solution {
     public static void main(String[] args) {
-        int a = 3126 - 8;
+        int a = 3126-8;
         System.out.println(a);
     }
 }
