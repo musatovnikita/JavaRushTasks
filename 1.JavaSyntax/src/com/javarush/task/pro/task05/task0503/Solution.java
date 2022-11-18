8124797b998f97cd089c35a795ed3c29df54edab
Nikitamusatov <nikitamusatov@gmail.com>
2022-11-18 07:32:26 +0300

    add solution JavaSyntax task 0503

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0503/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0503/Solution.java
@@ -11,7 +11,7 @@ public class Solution {
     public static void main(String[] args) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
-                array [i] *= -1;
+                array[i] *= -1;
             }
         }
         System.out.println(array[0]);
