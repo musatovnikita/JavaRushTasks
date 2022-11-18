d3c76326bc91561481944f377d6070b393c7bffc
Nikitamusatov <nikitamusatov@gmail.com>
2022-11-18 07:12:33 +0300

    add solution JavaSyntax task 0501

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0501/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0501/Solution.java
@@ -6,7 +6,7 @@ package com.javarush.task.pro.task05.task0501;
 
 public class Solution {
     public static void main(String[] args) {
-        int [] intArray = new int[10];
-        double [] doubleArray = new double[10];
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
     }
 }
